9a0fa7467b7c725952ed69f6992456dd9914df33
SarrD <leroidave@gmail.com>
2018-05-18 17:39:36 +0200

    Create AmisController.php

---
+++ src/AppBundle/Controller/AmisController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Utilisateur;
+
+class AmisController extends DefaultController
+{
+   /**
+   * @Route("/amis/{pseudo}", name="amis")
+   */
+   public function amis(Request $request, $pseudo )
+   {
+
+     $users = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateur');
+
+     $user = $users->findOneBy(['id' => $pseudo]);
+
+     if($user == null){
+       $this->redirectToRoute('profile_not_found');
+     }
+
+      $pageAmis = $this->render('pageAmis.html.twig', array(
+             'nom' => $user->getNom(),
+             'prenom'         => $user->getPrenom(),
+             'bio'         => $user->getBio(),
+             'id'         => $user->getId(),
+         ));
+
+         return $pageAmis;
+     }
+}
