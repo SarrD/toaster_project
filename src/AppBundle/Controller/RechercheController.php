5f98af1bfffe738745a18aa36236ea25ba07a1e0
Sarr <leroidave@gmail.com>
2018-05-20 17:10:56 +0200

    Correction recherche ( 'contains' + insensible à la case)
    Ex = on peut retrouver 'Jean Dupont' avec les mot clé "jea", "jean du", "JEAN DUPONT", etc...

--- src/AppBundle/Controller/RechercheController.php
+++ src/AppBundle/Controller/RechercheController.php
@@ -16,12 +16,24 @@ class RechercheController extends DefaultController
     public function recherche(Request $request){
 
        if ($request->getMethod() == "GET") {
-
+         /*
          $repo = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateur');
          $noms = $repo->findBy(['nom' => $request->get('recherche'), 'idRole' => 1]);
          $prenoms = $repo->findBy(['prenom' => $request->get('recherche'), 'idRole' => 1]);
          $tab_personnes = array_merge($noms, $prenoms);
          $tab_personnes = array_unique ($tab_personnes);
+         */
+         $query = $this->getDoctrine()->getManager()
+         ->createQuery("SELECT u.id id, u.prenom prenom, u.nom nom, u.ppPath ppPath
+                        FROM 'AppBundle:Utilisateur' u
+                        WHERE (LOWER(u.nom) LIKE :requete
+                        OR LOWER(u.prenom) LIKE :requete
+                        OR CONCAT(u.prenom, ' ', u.nom) LIKE :requete)
+                        AND u.idRole = 1
+                        ");
+         $query->setParameter('requete', '%'.strtolower(trim($request->get('recherche'))).'%');
+         $tab_personnes = $query->getArrayResult();
+
 
          $pageRecherche = $this->render('recherche.html.twig', array(
                 'personnes' => $tab_personnes,
